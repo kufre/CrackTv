346a739211d1b3a6dec6d3c2013b484ed3e1e738
Kufre Godwin <kufy201@gmail.com>
2018-08-21 10:34:30 +0100

    config file correction

--- config/config.js
+++ config/config.js
@@ -6,18 +6,18 @@ const mysql = require('mysql2');
 // get the promise implementation, we will use bluebird
 const bluebird = require('bluebird');
  
+// create the connection, specify bluebird as Promise
+const con =  mysql.createConnection({
+  host:'localhost', 
+  user: 'root', 
+  password: '12345',
+  database: 'on_CrackTv', 
+  Promise: bluebird
+});
 
 
 
 
-// const con =  mysql.createConnection({
-//   host: '108.61.47.186',
-//   port:  3306,
-//   user: 'innocrac', 
-//   password: 'limited2008',
-//   database: 'innocrac_movie', 
-//   Promise: bluebird
-// });
  
 
 
